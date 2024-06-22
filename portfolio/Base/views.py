cadae0677ad230cd6d8d8678a9d2129921175325
Umesh <umeshtamang129@gmail.com>
2024-06-22 18:57:05 +0545

    create a model and use a contact function in views and modify in home.html

--- portfolio/Base/views.py
+++ portfolio/Base/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from Base import models
+from Base.models import Contact 
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def contact(request):
+   if request.method=="POST":
+       print('post')
+       name=request.POST.get('name')
+       email=request.POST.get('email')
+       number=request.POST.get('number')
+       content=request.POST.get('content')
+       print(name,email,number,content )
