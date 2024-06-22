466094aa986e1d22146e314248db3a462877a9d0
Umesh <umeshtamang129@gmail.com>
2024-06-22 19:02:22 +0545

    modified in views.py

--- portfolio/Base/views.py
+++ portfolio/Base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from Base import models
 from Base.models import Contact 
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -14,3 +15,30 @@ def contact(request):
        number=request.POST.get('number')
        content=request.POST.get('content')
        print(name,email,number,content )
+
+       if len(name)>1 and len(name)<30:
+           pass
+       else:
+           messages.error(request,'Lenght of name should be greater than 2 and less than 30 words ')
+           return render(request,'home.html')
+       
+       if len (email)>1 and len(email)<30:
+           pass
+       else:
+           messages.error(request,'Invaild email try again ')
+           return render(request,'home.html')
+       print(len(number))
+       if  len(number)>9 and len(number)<13:
+           pass
+       else:
+           messages.error(request,'Invaild number please try again ')
+           return render(request,'home.html')
+       babu = models.Contact(name=name,email=email,content=content,number=number)
+       babu.save()
+       messages.success(request,'Thank You for contacting me!! Your message has been saved ')
+       print('Data has been saved to database')
+ 
+       print('No pass ')
+   return render(request,'home.html') 
+
+
